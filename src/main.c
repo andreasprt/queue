45bf5012581a350fa9b2b366f3f7686bb0302bcd
andreas <andreas.p.putra@polytron.co.id>
2025-05-26 19:51:12 +0700

    initial commit

---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include "include/queue.h"
+#include <stdio.h>
+
+#define MAX_QUEUE_SIZE 10
+
+int main() {
+    // Inisialisasi queue
+    Queue_t* myQueue = QUEUE_Create(MAX_QUEUE_SIZE);
+    
+    if (myQueue == NULL) {
+        printf("Gagal membuat queue!\n");
+        return -1;
+    }
+    
+    // Contoh data
+    Data_t data1 = {1, "Data1", 85.5f};
+    Data_t data2 = {2, "Data2", 92.0f};
+    Data_t data3 = {3, "Data3", 78.3f};
+    
+    // Enqueue data
+    if (!QUEUE_Enqueue(myQueue, data1)) {
+        printf("Gagal enqueue data1!\n");
+    }
+    if (!QUEUE_Enqueue(myQueue, data2)) {
+        printf("Gagal enqueue data2!\n");
+    }
+    if (!QUEUE_Enqueue(myQueue, data3)) {
+        printf("Gagal enqueue data3!\n");
+    }
+    
+    // Tampilkan ukuran queue
+    printf("Ukuran queue: %d\n", QUEUE_Size(myQueue));
+    
+    // Dequeue dan tampilkan data
+    Data_t temp;
+    while (QUEUE_Dequeue(myQueue, &temp)) {
+        printf("Dequeue: ID=%d, Nama=%s, Nilai=%.1f\n", 
+               temp.id, temp.nama, temp.nilai);
+    }
+    
+    // Bersihkan queue
+    QUEUE_Clear(myQueue);
+    
+    return 0;
+}
